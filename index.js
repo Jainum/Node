5e2e5b5a3fec3fdf9be6e507593a77f4a8e7bf06
Jainum <55753045+Jainum@users.noreply.github.com>
2020-04-22 08:59:00 -0400

    Added a Node API Project

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+
+//Import Routes
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+
+dotenv.config();
+
+//Connect to Database
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  console.log('DB Connected')
+);
+
+//Middlewares
+app.use(express.json());
+
+//Route Middlewares
+app.use('/api/user', authRoute);
+app.use('/api/posts', postRoute);
+
+app.listen(3000, () => console.log('Server up and running'));
